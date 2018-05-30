cd22592bb5ed0af45083e2635a06760fec456a20
Mahmoud Khaled <mahmoudkhaled@Mahmouds-Mac.local>
2018-05-30 02:30:01 +0200

    minor change

---
+++ app-swoosh/app-swoosh/Viwes/BorderButtons.swift
@@ -0,0 +1,21 @@
+//
+//  BorderButtons.swift
+//  app-swoosh
+//
+//  Created by Mahmoud Khaled on 5/29/18.
+//  Copyright © 2018 Mahmoud Khaled. All rights reserved.
+//
+
+import UIKit
+
+class BorderButtons: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
